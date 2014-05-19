56beb3ee25ca15bb7427c46001946fda93bc1b21
nachivpn <nachivpn@gmail.com>
2014-05-19 20:33:50 +0800

    updated with some fixes

--- cfu.sh
+++ cfu.sh
@@ -1,42 +1,47 @@
 ##!/bin/bash
 
 file1="index.html"
+file2="old_index.html"
 link_file1="link"
 link_file2="old_link"
 
-#clear screen
 clear
+
 #downloading index.html
 if [ ! -f "$file1" ]
 then
-	echo "index file does not already exist"
 	echo "attempting to download index file..."
-	wget -q -t inf "http://www.nitc.ac.in/academics/Results_May_2014/"
+	wget -t inf "http://www.nitc.ac.in/academics/Results_May_2014/"
+else
+	echo "index file found"
 fi
 
 #extracting required link
 echo "extracting link.."
-cat index.html | grep cse/S8 > $link_file2
+cat "$file1" | grep "cse/S8" > "$link_file2"
 
 #duplicate link file
-cp $link_file2 $link_file1
+cp "$link_file2" "$link_file1"
 
 #check for updates
-for x in `seq 1 50`
+for x in `seq 1 1000` 
 do
-	echo "Comparison number: "$x
+	echo "Comparison number: ""$x"
 	if cmp -s "$link_file1" "$link_file2"
 	then
-    		echo "No update. sigh."
+  	echo "No update. sigh."
 		echo "Trying again..."
-		rm -f index.html
-   		wget -q -t inf "http://www.nitc.ac.in/academics/Results_May_2014/"
-		mv $link_file1 $link_file2
-		cat index.html | grep cse/S8 > $link_file1
+		rm -f "$file2"
+		mv "$file1" "$file2"
+		rm -f "$file1"
+  	wget -q -t inf "http://www.nitc.ac.in/academics/Results_May_2014/"
+		rm -f "$link_file2"
+		mv "$link_file1" "$link_file2"
+		cat index.html | grep "cse/S8" > "$link_file1"
 	else
-    		echo "Some update!"
+  	echo "--------------------------Some update!---------------------------"
 		echo "Displaying cse/S8 link:" 
-    		cat $link_file1 
+  	cat "$link_file1" 
 		break
 	fi
 done
